b69fc9208363a1fd6fafc82b209e1a0a9f4691ae
wenchen0511 <wc21511@163.com>
2018-05-14 23:47:49 +0800

    会员等级删除api对应。

--- src/page/member/level.vue
+++ src/page/member/level.vue
@@ -65,6 +65,29 @@
           </div>
         </div>
       </div>
+          <div id="deleteConfirmMdoal" class="modal fade" aria-hidden="true" style="display: none;">
+      <div class="modal-dialog modal-md">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+            <h4 class="modal-title">温馨提示</h4>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="alert alert-danger">
+                  确定要删除吗？
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" @click="realRemove" class="btn btn-primary">确定</button>
+            <button type="button" class="btn btn-white" data-dismiss="modal">取消</button>
+          </div>
+        </div>
+      </div>
+    </div>
       </div>
    </div>
 </template>
@@ -170,7 +193,23 @@ export default {
     },
     removeLevel:function(index){
        let _this = this;
-       _this.levelList.splice(index,1);
+       _this.curIndex = index;
+       if(_this.levelList[index].id) {
+          $("#deleteConfirmMdoal").modal('show');
+       } else {
+        _this.levelList.splice(index,1);
+       }
+    },
+    realRemove: function(){
+      let _this = this;
+      _this.$axios.delete('levels/' + _this.levelList[_this.curIndex].id).
+        then((result)=> {
+          _this.levelList.splice(_this.curIndex,1);
+          $("#deleteConfirmMdoal").modal('hide');
+          _this.SHIFT_LOADING();
+      }).catch((err) => {
+          _this.SHIFT_LOADING();
+      });
     },
     levelSubmit:function(){
       let _this = this; // 验证
